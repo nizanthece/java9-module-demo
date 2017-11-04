b6fd3755d8d4cb8c0d024afd1a941baad08d02a4
nizanthece <nizanthece@gmail.com>
2017-11-04 00:23:16 -0400

    java module demo

---
+++ src/com/mymodule/hello/Hello.java
@@ -0,0 +1,7 @@
+package com.mymodule.hello;
+
+public class Hello {
+    public void helloJavaModule() {
+        System.out.println("Java 9:Module:Hello.............");
+    }
+}
---
+++ src/org/mydemo/test/moduleTest.java
@@ -0,0 +1,11 @@
+package org.mydemo.test;
+
+import com.mymodule.hello.Hello;
+
+public class moduleTest {
+    public static void main(String[] args) {
+       Hello hello = new Hello();
+       hello.helloJavaModule();
+    }
+
+}
